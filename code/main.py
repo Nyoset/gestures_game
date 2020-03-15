9d37709eede606b7ec40619e7dece6453119ee9d
Marc Basquens <marc.basquens@basetis.com>
2020-03-15 16:44:28 +0100

    Add saving and loading

--- code/main.py
+++ code/main.py
@@ -6,14 +6,16 @@ import numpy as np
 from os import listdir, scandir, environ
 import os
 import os.path as path
+import argparse
 
 import imageio
 import glob
 
 dir_path = path.dirname(path.realpath(__file__))
 
+save_path = path.join(dir_path, os.pardir, "model")
 data_path = path.join(dir_path, os.pardir, "shapes")
-image_size = 120
+image_size = 24
 
 
 def show_image(image):
@@ -39,23 +41,28 @@ def read_data():
     categories_data = []
     categories = [f.path for f in scandir(data_path) if f.is_dir()]
 
-    for category in categories:
-        for im_path in glob.glob(path.join(data_path, category, '*.png')):
-            image = imageio.imread(im_path)
-            saturated_image = remove_channels(image)
-            shapes_data.append(saturated_image)
-            categories_data.append(encode(trim_label(category)))
+    train_image_generator = keras.preprocessing.image.ImageDataGenerator(rescale=1./255,
+                                                                         zoom_range=0.2,
+                                                                         rotation_range=5,
+                                                                         horizontal_flip=True)
 
-    return np.array(categories_data), np.array(shapes_data)
+    train_data = train_image_generator.flow_from_directory(batch_size=2,
+                                                           directory=data_path,
+                                                           shuffle=True,
+                                                           target_size=(image_size, image_size))
+
+    return train_data
 
 
 def encode(name):
     return 0 if name == 'circle' else 1
 
 
-def train(labels, images):
+def train(data):
     model = keras.Sequential([
-        keras.layers.Dense(units=256, activation='relu', input_shape=(image_size, image_size)),
+        keras.layers.Dense(units=256, activation='relu', input_shape=(image_size, image_size, 3)),
+        keras.layers.MaxPool2D((2, 2)),
+        keras.layers.Conv2D(2, 2, activation='relu'),
         keras.layers.Flatten(),
         keras.layers.Dense(units=2, activation='softmax')
     ])
@@ -64,15 +71,29 @@ def train(labels, images):
                   loss=losses.CategoricalCrossentropy(from_logits=True),
                   metrics=['accuracy'])
 
-    history = model.fit(
-        images,
-        keras.utils.to_categorical(labels)
+    history = model.fit_generator(
+        data
     )
 
-    print(model.predict(images, batch_size=64)[0])
+    model.save(save_path)
+    model.summary()
+
+
+def predict():
+    model = keras.models.load_model(save_path)
+    model.summary()
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='TODO: Write HELP')
+
+    parser.add_argument('-t', '--train', dest='train', action='store_true')
+    args = parser.parse_args()
+
     environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-    labels, images = read_data()
-    train(labels, images)
+
+    if args.train:
+        data = read_data()
+        train(data)
+    else:
+        predict()
